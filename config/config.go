a69b2d14bc2416c3593f65e59060a954da5780ba
Andrew Brighton <snatvb@ya.ru>
2023-06-25 03:59:30 +0300

    Updated timings to actual values

--- config/config.go
+++ config/config.go
@@ -2,20 +2,20 @@ package config
 
 import "time"
 
-// var timings = map[int]time.Duration{
-// 	0: 1 * time.Hour,
-// 	1: 12 * time.Hour,
-// 	2: 24 * time.Hour,
-// 	3: 36 * time.Hour,
-// 	4: 48 * time.Hour,
-// 	5: 72 * time.Hour,
-// }
-
 var Timings = map[int]time.Duration{
-	0: 1 * time.Minute / 2,
-	1: 1 * time.Minute,
-	2: 1 * time.Minute,
-	3: 1 * time.Minute,
-	4: 1 * time.Minute,
-	5: 1 * time.Minute,
+	0: 1 * time.Hour,
+	1: 12 * time.Hour,
+	2: 24 * time.Hour,
+	3: 36 * time.Hour,
+	4: 48 * time.Hour,
+	5: 72 * time.Hour,
 }
+
+// var Timings = map[int]time.Duration{
+// 	0: 1 * time.Minute / 2,
+// 	1: 1 * time.Minute,
+// 	2: 1 * time.Minute,
+// 	3: 1 * time.Minute,
+// 	4: 1 * time.Minute,
+// 	5: 1 * time.Minute,
+// }
